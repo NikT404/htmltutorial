74a30671a946ac1992c009bb891e8f7d96fccf68
NikT404 <nikhilsinghrajawat0@gmail.com>
2024-07-25 16:15:36 +0530

    primitive and non pritive

--- 01class/index.js
+++ 01class/index.js
@@ -1,3 +1,23 @@
-var name="hello"
-console.log(name);
+// var name="hello"
+// console.log(name);
 
+// let name1="we are brave"
+// console.log(name1);
+
+
+// var num1 =1000
+// var num=700.55
+// console.log(num);
+
+// let num5=3456.889
+// console.log(num5);
+
+// let name=hello
+// console.log(typeof name)
+// console.log(name)
+
+
+
+let bool=true
+console.log(typeof bool);
+console.log(bool);
---
+++ 02class/index.js
@@ -0,0 +1,81 @@
+// console.log("utkarsh");
+// var name="hello"
+// name="utkarsh"
+// console.log(name);
+
+
+// let name1="hello1"
+// name1="world1"
+// console.log(name1);
+// const ke saath koi  change nhi kr skta 
+// const name2="hello2"
+// name2="world2"
+// console.log(name2);
+
+// changes are available
+// var name1="hello1"  
+// var name1="world1"  
+// console.log(name1);
+
+
+// Identifier 'name2' has already been declared
+// let name2="hello2"
+// let name2="world2"
+// console.log(name2);
+
+
+
+// const name3 = "world3"
+//     ^
+
+//     SyntaxError: Identifier 'name3' has already been declared
+
+
+
+// const name3 = "hello3"
+// const name3 = "world3"
+// console.log(name3);
+
+
+// SyntaxError: Identifier 'name1' has already been declared
+// var name1="hello1"
+// let name1="hello2"
+// console.log(name1);
+
+
+// const obj={name:"utkarsh","age":20,"is alive":true}
+// console.log(obj);
+
+
+// particular value ko aceess krne ke liye 
+// console.log(obj["name"])
+// console.log(obj["age"])
+
+// console.log(obj["is alive"])
+
+// const obj1 = { "first name": "utkarsh", "last name": "singh", "address": "jaipur" };
+// console.log(obj1);
+// obj1["address"] = "ajmer";
+// console.log(obj1);
+// its mutable because its primitve in nature
+
+// same as above  one 
+// console.log(obj1["first name"])
+// console.log(obj1["last name"])
+// console.log(obj1["address"])
+
+const arr =["utk",24,"jaipur",true]
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+
+
+// let num1=10
+
+// let num2=20
+// console.log(num1+num2);
+
+
+
