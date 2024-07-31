5b99f88acf17293409b4c8290fa6dd0b0be0c2c4
NikT404 <nikhilsinghrajawat0@gmail.com>
2024-07-31 14:56:54 +0530

    about the js

---
+++ 04class/index.js
@@ -0,0 +1,97 @@
+// let num1=10
+// let num2=20
+// console.log(num2>num1);
+// console.log(num1>num2);
+
+// let num1=10
+// let num2=10
+// console.log(num1==num2);
+
+// let num1=10
+// let num2=11
+// console.log(num1==num2);
+
+// let num1=10
+// let num2=10
+// console.log(num1==num2);
+// console.log(num1===num2);
+// console.log(typeof num1);
+// console.log(typeof num2);
+// questions on the assignment operator
+// let num1=20
+// let num2=18
+
+// num1+=2
+// answer
+// 21
+// 18
+// console.log(num1);
+// console.log(num2);
+
+
+// num1-=2
+// console.log(num1);
+// console.log(num2);
+// aanswer
+// 17
+// 18
+ 
+// num1*=2
+//  console.log(num1);
+//  console.log(num2);
+// aanswer
+//  38
+//  18
+
+// num1/=2
+//   console.log(num1);
+//   console.log(num2);
+// answer
+// 10
+// 18
+
+
+// num1!=2
+//  console.log(num1);
+//  console.log(num2);
+// answer
+//  20
+// let num1=10
+// let num2=20
+// let num3=30
+// console.log(num1>num2&&num1>num3);
+// console.log(num2>num1&&num3>num2);
+// console.log(num1>num2&&num1>num3);
+// console.log(num1>num2&&num1>num3);
+
+// let num2=20
+// let num3=30
+// let result=(num2>num3)? "yes":"no"
+// console.log(result);
+// if the yes and no are not in the ""  then it will give the error 
+
+
+
+// let num1 = 10
+// let num2 = 20
+// let num3 = 30
+// let result = (num1 > num2) ? (num1 > num3 ? num1 : num3) : (num2 > num3 ? num2 : num3);
+// console.log(result);
+
+// let num1= 100.556
+// let num2= `100.556`
+// // 0 ki value false hoti hai
+// console.log((!num1)?"yes":"no");
+
+// let num1=undefined
+// ''-> iska mtbl iski value 0 hai to no de rha hai 
+// undefined null ki trha kaam krta hai
+// let num1=[]
+
+// console.log((num1)?"yes":"no")
+
+// let num1=[true,{age:25}]
+
+// console.log((num1[1]["age"])?"yes":"no")
+
+
